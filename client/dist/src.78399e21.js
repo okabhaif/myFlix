b1f69c3296da150ab980234bb8c83952c376fac3
okabhaif <okabhaif@users.noreply.github.com>
2020-03-16 11:23:29 +0000

    Updated
    removed an accidental duplication of a file from components

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -32908,7 +32908,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63967" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61988" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
