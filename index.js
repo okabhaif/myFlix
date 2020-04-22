bb90413d6a292e68cb99633e769120bb39e870bd
okabhaif <okabhaif@users.noreply.github.com>
2020-04-22 13:41:39 +0100

    update
    Cors bug

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { check, validationResult } = require('express-validator');
 
 //mongoose.connect('mongodb://localhost:27017/myFlixDb', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-var allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflix-project.herokuapp.com/'];
+var allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflix-project.herokuapp.com'];
 
 //middleware.
 app.use(cors({
